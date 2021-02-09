140900d70633ebd196a1dac07b73a5cdff0caa8b
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-09 22:47:36 +0000

    text files

---
+++ 17-camelforth-a.d/doc/quickvocab.fs
@@ -0,0 +1,8 @@
+( quickvocab.fs )
+HEX
+: dump DUMP ; : here HERE ; : words WORDS ; 
+: cold COLD ; : .s .S ; : hex HEX ; 
+: decimal DECIMAL ; : cr CR ; 
+: swap SWAP ; : drop DROP ; : dup DUP ; 
+: over OVER ; 
+( end of list )
