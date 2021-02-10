f28194fbf8810352b1cec54928400bda848abb01
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-10 01:37:14 +0000

    Forth vocab quick.
    BUG: @@@ thing not showing now, decent long
    session in the interpreter.
    
    Call COLD more often - and then, not.
    
    Compare between them.
    
    	modified:   quickvocab.fs
    
    On branch dvlp-aa-

--- 17-camelforth-a.d/doc/quickvocab.fs
+++ 17-camelforth-a.d/doc/quickvocab.fs
@@ -5,4 +5,10 @@ HEX
 : decimal DECIMAL ; : cr CR ; 
 : swap SWAP ; : drop DROP ; : dup DUP ; 
 : over OVER ; 
+: p 18 dump dup cr . ;
+: q 3000 + p ; ( jump way ahead )
+( interesting addresses )
+: cpyaa 10066E20 ; ( ascii strings circuitpython )
+( remnant remaining in the QSPI flashROM )
+: cpybb 1006A6D0 ; ( end of ascii string stuff )
 ( end of list )
