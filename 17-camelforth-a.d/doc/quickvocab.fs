95025d8883577d0c42dc8c1ce917c426e5dddc1a
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-11 05:10:59 +0000

    edits to quick vocab sheet

--- 17-camelforth-a.d/doc/quickvocab.fs
+++ 17-camelforth-a.d/doc/quickvocab.fs
@@ -2,11 +2,13 @@
 HEX
 : dump DUMP ; : here HERE ; : words WORDS ; 
 : cold COLD ; : .s .S ; : hex HEX ; 
-: decimal DECIMAL ; : cr CR ; 
-: swap SWAP ; : drop DROP ; : dup DUP ; 
-: over OVER ; 
-: p 18 dump dup cr . ;
+: decimal DECIMAL ; : cr CR ; : swap SWAP ;
+: drop DROP ; : dup DUP ; : over OVER ;
+: p 18 dump dup cr . ; ( maximum dump single screenful )
 : q 3000 + p ; ( jump way ahead )
+: tryloop ( hi lo -- ) ( ex. 4 0 tryloop )
+  DO blink LOOP ;
+(                       )
 ( interesting addresses )
 : cpyaa 10066E20 ; ( ascii strings circuitpython )
 ( remnant remaining in the QSPI flashROM )
