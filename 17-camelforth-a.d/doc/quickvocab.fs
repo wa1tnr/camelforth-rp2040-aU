ef9c7d2d23682911764f11bd3db2b7f53330670f
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-11 19:17:47 +0000

    delay word in tenths of a second
    	modified:   quickvocab.fs
    
    On branch dvlp-aa-

--- 17-camelforth-a.d/doc/quickvocab.fs
+++ 17-camelforth-a.d/doc/quickvocab.fs
@@ -4,10 +4,14 @@ HEX
 : cold COLD ; : .s .S ; : hex HEX ; 
 : decimal DECIMAL ; : cr CR ; : swap SWAP ;
 : drop DROP ; : dup DUP ; : over OVER ;
+: space SPACE ; : spaces SPACES ; : emit EMIT ;
 : p 18 dump dup cr . ; ( maximum dump single screenful )
 : q 3000 + p ; ( jump way ahead )
 : tryloop ( hi lo -- ) ( ex. 4 0 tryloop )
   DO blink LOOP ;
+: delay ( n -- ) ( tenths of a second )
+  1D000 *
+  0 DO 1 DROP LOOP ;
 (                       )
 ( interesting addresses )
 : cpyaa 10066E20 ; ( ascii strings circuitpython )
