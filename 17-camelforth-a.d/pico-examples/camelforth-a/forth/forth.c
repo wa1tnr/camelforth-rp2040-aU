9d172ed4e1df1103f3976494cd139920534553dd
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-14 22:53:44 +0000

    timestamp of good version of CamelForth

--- 17-camelforth-a.d/pico-examples/camelforth-a/forth/forth.c
+++ 17-camelforth-a.d/pico-examples/camelforth-a/forth/forth.c
@@ -1,4 +1,4 @@
-#define VERS_CFORTH ("\103CamelForth in C v0.1 - 14 Feb 2016 - Sun Feb 14 16:09:50 UTC 2021  ");
+#define VERS_CFORTH ("\103CamelForth in C v0.1 - 14 Feb 2016 - Sun Feb 14 22:52:26 UTC 2021  ");
 // special attempt: make some pointerish things more robust by superstitiously using 'volatile' all over the place ;)
 // surprisingly, all these changes in this commit do compile cleanly.
 /****h* camelforth/forth.c
