9764a5a96f79c5adbbe2b7b718a4d9c1e95d24d6
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-17 20:46:06 +0000

    Decent LED operational words for GP25
    These all worked correctly:
    
    ok LED_HI
    ok LED_LOW
    ok LED_HI
    ok LED_LOW
    ok LED_HI
    ok LED_INPUT
    ok LED_OUTPUT
    
    Could not test if INPUT and OUTPUT toggled
    exactly correctly, but the port did behave
    a lot 'as if' they had behaved so - in
    specific, the LED lit only during OUTPUT and
    this state toggle-able using these two words.
    
    Thus giving the appearance that both words
    did something real.
    
    	modified:   gpio.fs
    
    On branch dvlp-aa-

--- doc/gpio.fs
+++ doc/gpio.fs
@@ -5,6 +5,7 @@
 : LEDG ( -- adrs ) PADS_BANK0_BASE GPIO25 + ;
 : GPIO_OUT ( -- n ) 10 ; ( bits 29:0 per GPIO )
 : GPIO_OUT_SET ( -- n ) 14 ; ( Table 22 )
+: GPIO_OUT_CLR ( -- n ) 18 ; ( 2.3.1.7 List of Registers )
 ( 1 7 LSHIFT gives 128 )
 ( 1  2  4  8  16  32  64  128 )
 ( 0  1  2  3   4   5   6    7 )
@@ -13,20 +14,39 @@
 
 : P25 19 ( -- n ) ; ( GPIO25 0x19 is 25 decimal )
 : LED_OUT SIO_BASE GPIO_OUT + ( -- addr ) ;
+
+( LED_OUTPUT means place the port pin in OUTPUT mode )
 : LED_OUTPUT ( -- )
   LED_OUT @ ( -- n )
   1 P25 LSHIFT OR LED_OUT !
 ;
-: LED_INPUT_UNTEST ( -- ) ( not tested and wrong )
+( LED_INPUT means place the port pin in INPUT mode )
+: LED_INPUT ( -- )
   LED_OUT @
-  1 P25 LSHIFT NEGATE AND LED_OUT !
+  1 P25 LSHIFT INVERT AND LED_OUT !
 ;
+( just an address for LED_OUT_SET )
 : LED_OUT_SET SIO_BASE GPIO_OUT_SET + ( -- addr ) ;
-( the ONLY tested word was 'LED_HI' and it did work - LED turned on )
+: LED_OUT_CLR SIO_BASE GPIO_OUT_CLR + ( -- addr ) ;
+
+( turn on the port pin for the LED with LED_HI )
 : LED_HI ( -- )
   LED_OUT_SET @ ( -- n ) ( gave 0x56 = 86 decimal )
   (  0 1 0 1   0 1 1 0 ) ( 86 decimal )
   1 P25 LSHIFT
-  OR LED_OUT_SET !
-;
+  OR LED_OUT_SET ! ;
+
+: LED_LOW_NOPE ( -- ) ( do NOT use )
+  LED_OUT_CLR @
+  1 P25 LSHIFT INVERT
+  AND LED_OUT_CLR ! ;
+
+: LED_LOW ( -- )
+  LED_OUT_CLR @
+  1 P25 LSHIFT
+  OR LED_OUT_CLR ! ;
+
+(  2000000 19 RSHIFT . 1  ok ) 
+(  1000000 18 RSHIFT . 1  ok )
+
 ( end )
